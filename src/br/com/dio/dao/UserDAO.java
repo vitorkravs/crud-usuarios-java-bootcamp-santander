01233cb51668efe82893f10cce7346a609dde35d
Vitor Kravszenko <vitor.kravs@accist.com.br>
2025-06-23 23:55:15 -0300

    feat: criar os controllers do crud do projeto

---
+++ src/br/com/dio/dao/UserDAO.java
@@ -0,0 +1,46 @@
+package br.com.dio.dao;
+
+import br.com.dio.exception.UserNotFoundException;
+import br.com.dio.model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+
+    private long nextId = 1;
+
+    private List<UserModel> models = new ArrayList<>();
+
+    public UserModel save(final UserModel model){
+        model.setId(nextId++);
+        models.add(model);
+        return model;
+    }
+
+    public UserModel update(final UserModel model) {
+        var toUpdate = findById(model.getId());
+
+        models.remove(toUpdate);
+        models.add(model);
+        return model;
+    }
+
+    public void delete(final long id) {
+        var toDelete = findById(id);
+        models.remove(toDelete);
+    }
+
+    public UserModel findById(final long id) {
+        var message = String.format("Não existe usúario com o id %s cadastrado!", id);
+        return  models.stream()
+                .filter(u ->  u.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException(message));
+    }
+
+    public List<UserModel> findAll(){
+        return models;
+    }
+
+}
